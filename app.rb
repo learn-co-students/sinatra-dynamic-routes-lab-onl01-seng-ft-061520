966e5df72927a24ff2d276b62297c77204c8d33f
kak79 <kohelka@gmail.com>
2020-08-02 13:43:06 -0500

    Done.

--- app.rb
+++ app.rb
@@ -7,27 +7,36 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    (params[:number].to_i **2).to_s 
+    (params[:number].to_i ** 2).to_s 
   end
 
   get '/say/:number/:phrase' do
-    
-    
+    result = ''
+    params[:number].to_i.times do
+      result += params[:phrase]
+    end
+    result
   end  
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    "#{params[:word1]} +" "+ #{params[:word2]} +" "+ #{params[:word3]} +" "+ #{params[:word4]} +" "+ #{params[:word5]}."
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
 
   get '/:operation/:number1/:number2' do
-    @operation = params[:number1].to_i + params[:number2].to_i
-    "#{@operation}"   
-    @operation = params[:number1].to_i - params[:number2].to_i 
-    "#{@operation}"   
-    @operation = params[:number1].to_i * params[:number2].to_i
-    "#{@operation}"   
-    @operation = params[:number1].to_i / params[:number2].to_i
-    "#{@operation}"    
-  end  
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
 
+    answer = 'Unable to perform this operation'
+
+    case params[:operation]
+    when 'add'
+      answer = (number1 + number2).to_s
+    when 'subtract'
+      answer = (number1 - number2).to_s
+    when 'multiply'
+      answer = (number1 * number2).to_s
+    when 'divide'
+      answer = (number1 / number2).to_s
+    end
+  end
 end
